3701d27a9acd1f694c9b256a51180d2063caf491
Anton Jurisevic <anton@block8.io>
2018-08-15 16:53:39 +1000

    Add ERC20 interface contract.

---
+++ contracts/interfaces/ERC20Token.sol
@@ -0,0 +1,15 @@
+contract ERC20Token {
+    string public constant name = "Token Name";
+    string public constant symbol = "SYM";
+    uint8 public constant decimals = 18;
+
+    function totalSupply() public view returns (uint);
+    function balanceOf(address user) public view returns (uint);
+    function allowance(address owner, address spender) public view returns (uint);
+    function approve(address spender, uint quantity) public returns (bool);
+    function transfer(address to, uint quantity) public returns (bool);
+    function transferFrom(address from, address to, uint quantity) public returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint quantity);
+    event Approval(address indexed owner, address indexed spender, uint quantity);
+}
