214d706e75fe50e6e79d4227becdc1dd5008ef87
Anton Jurisevic <anton@block8.io>
2018-08-16 12:02:04 +1000

    Added a iterable set to clean up the contract itself.

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -2,17 +2,26 @@ pragma solidity ^0.4.24;
 pragma experimental "v0.5.0";
 
 import "./interfaces/ERC20Token.sol";
+import "./utils/IterableSet.sol";
 
 // All fixed point math is done to 18 decimal places.
-contract AkropolisFund {
+contract AkropolisFund is IterableSet {
     address board; // TODO: This should be of type Board, once we merge in the `board-of-directors` branch.
     address manager;
 
-    uint managementFee;
-    // Percentage of profits per time?
-    // Is it a percentage of AUM?
-    // Per something else? Is it a flat rate?
-    // TODO: Confirm these details with ana.
+    // Percentage of AUM over one year.
+    // TODO: Add a flat rate as well. Maybe also performance fees.
+    uint managementFeePerYear;
 
-    ERC20Token[] approvedTokens;
+    // Tokens that this fund is approved to own.
+    Set approvedTokens;
+
+    // Token in which benefits will be paid.
+    ERC20Token quoteAsset;
+    
+    // Each user has a time after which they can withdraw benefits. Can be modified by fund directors.
+    mapping(address => uint) userTimeLock;
+
+    Set members;
+
 }
