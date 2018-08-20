39c1961e7051ac248a0b75d37f6def17f933be29
Joel Aquilina <loadedice@users.noreply.github.com>
2018-08-20 16:43:45 +1000

    Add fundobjects contract

---
+++ contracts/utils/FundObjects.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+pragma experimental "v0.5.0";
+
+import "../interfaces/ERC20Token.sol";
+
+// Abstract contract
+contract FundObjects {
+
+    struct JoinRequest {
+        uint unlockTime;
+        ERC20Token[] tokens;
+        uint[] contributions;
+        uint expectedShares;
+        bool complete;
+    }
+}
