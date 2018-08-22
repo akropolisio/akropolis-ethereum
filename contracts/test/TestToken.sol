ce1b10288e01060d0a17dcc1174e2fe73f1dd1a0
Joel Aquilina <loadedice@users.noreply.github.com>
2018-08-22 13:16:39 +1000

    Add in test ERC20 token

---
+++ contracts/test/TestToken.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.24;
+pragma experimental "v0.5.0";
+
+import "../interfaces/ERC20Token.sol";
+import "../utils/SafeDecimalMath.sol";
+
+contract TestToken is ERC20Token, SafeDecimalMath {
+
+    string public symbol;
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    constructor(string _symbol, uint total) public {
+        symbol = _symbol;
+        totalSupply = total * 10**uint(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowance[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], tokens);
+        balanceOf[to] = safeAdd(balanceOf[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balanceOf[from] = safeSub(balanceOf[from], tokens);
+        allowance[from][msg.sender] = safeSub(allowance[from][msg.sender], tokens);
+        balanceOf[to] = safeAdd(balanceOf[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+}
