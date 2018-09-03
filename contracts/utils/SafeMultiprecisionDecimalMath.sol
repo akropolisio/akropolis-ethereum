4358d068bec7bd41813a99fb2e4672be4fec7e4e
Anton Jurisevic <anton@block8.io>
2018-09-03 12:17:17 +1000

    Replace math library with multi-precision variant.

--- contracts/utils/SafeMultiprecisionDecimalMath.sol
+++ contracts/utils/SafeMultiprecisionDecimalMath.sol
@@ -3,14 +3,11 @@
 FILE INFORMATION
 -----------------------------------------------------------------
 
-file:       SafeDecimalMath.sol
+file:       SafeMultiprecisionDecimalMath.sol
 version:    1.0
 author:     Anton Jurisevic
 
 date:       2018-2-5
-
-auditors: Sigma Prime, https://github.com/sigp/havven-audit
-          BlocTrax, https://havven.io/uploads/havven_bloctrax_security_audit_june-6th.pdf
 
 -----------------------------------------------------------------
 MODULE DESCRIPTION
@@ -18,10 +15,12 @@ MODULE DESCRIPTION
 
 A fixed point decimal library that provides basic mathematical
 operations, and checks for unsafe arguments, for example that
-would lead to overflows.
+would lead to overflows. Exceptions are thrown whenever those
+unsafe operations occur.
 
-Exceptions are thrown whenever those unsafe operations
-occur.
+The library has no fixed precision, and all relevant operations
+must be provided with the input and output precision of the
+operands.
 
 -----------------------------------------------------------------
 */
@@ -35,13 +34,15 @@ pragma experimental "v0.5.0";
  * @dev Functions accepting uints in this contract and derived contracts
  * are taken to be such fixed point decimals (including fiat, ether, and nomin quantities).
  */
-contract SafeDecimalMath {
+contract SafeMultiprecisionDecimalMath {
 
-    /* Number of decimal places in the representation. */
-    uint8 public constant decimals = 18;
-
-    /* The number representing 1.0. */
-    uint public constant UNIT = 10 ** uint(decimals);
+    function unit(uint decimals)
+        pure
+        internal
+        returns (uint)
+    {
+        return 10 ** decimals;
+    }
 
     /**
      * @return True iff adding x and y will not overflow.
@@ -119,6 +120,18 @@ contract SafeDecimalMath {
         return p;
     }
 
+    function convertPrecision(uint x, uint decimalsFrom, uint decimalsTo)
+        pure
+        internal
+        returns (uint)
+    {   
+        // The guard allows us to eschew safeSub();
+        if (decimalsFrom > decimalsTo) {
+            return safeMul(x, unit(decimalsFrom - decimalsTo));
+        }
+        return safeDiv(x, unit(decimalsTo - decimalsFrom));
+    }
+
     /**
      * @return The result of multiplying x and y, interpreting the operands as fixed-point
      * decimals. Throws an exception in case of overflow.
@@ -131,14 +144,12 @@ contract SafeDecimalMath {
      * contain small enough fractional components. It would also marginally diminish the 
      * domain this function is defined upon. 
      */
-    function safeMul_dec(uint x, uint y)
+    function safeMul_mpdec(uint x, uint xDecimals, uint y, uint yDecimals, uint outDecimals)
         pure
         internal
         returns (uint)
     {
-        /* Divide by UNIT to remove the extra factor introduced by the product. */
-        return safeMul(x, y) / UNIT;
-
+        return convertPrecision(safeMul(x, y), xDecimals + yDecimals, outDecimals);
     }
 
     /**
@@ -172,50 +183,24 @@ contract SafeDecimalMath {
      * @dev Throws an exception in case of overflow or zero divisor; x must be less than 2^256 / UNIT.
      * Internal rounding is downward: a similar caveat holds as with safeMul_dec().
      */
-    function safeDiv_dec(uint x, uint y)
+    function safeDiv_mpdec(uint x, uint xDecimals, uint y, uint yDecimals, uint outputDecimals)
         pure
         internal
         returns (uint)
     {
-        /* Reintroduce the UNIT factor that will be divided out by y. */
-        return safeDiv(safeMul(x, UNIT), y);
+        /* Reintroduce the yUnit factor that will be divided out by y. */
+        return convertPrecision(safeDiv(safeMul(x, unit(yDecimals)), y), xDecimals, outputDecimals);
     }
 
     /**
      * @dev Convert an unsigned integer to a unsigned fixed-point decimal.
      * Throw an exception if the result would be out of range.
      */
-    function intToDec(uint i)
+    function intToDec(uint i, uint decimals)
         pure
         internal
         returns (uint)
     {
-        return safeMul(i, UNIT);
+        return safeMul(i, unit(decimals));
     }
 }
-
-/*
------------------------------------------------------------------------------
-MIT License
-
-Copyright (c) 2018 Havven
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
------------------------------------------------------------------------------
-*/
