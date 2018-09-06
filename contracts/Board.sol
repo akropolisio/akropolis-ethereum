af98f0769edafb4b059e30c105a34628e40b753d
Anton Jurisevic <anton@block8.io>
2018-09-06 14:17:44 +1000

    Fix board tests broken by motion description requirement.

--- contracts/Board.sol
+++ contracts/Board.sol
@@ -194,7 +194,7 @@ contract Board is BytesHandler {
         returns (uint)
     {
         require(data.length > 0, "Data must not be empty.");
-        require(bytes(description).length > 0, "No description provided.");
+        require(bytes(description).length > 0, "Description must be provided.");
         uint id = _pushMotion(motionType, MotionStatus.Active, msg.sender,
                               duration, 0, 0, description, data);
         emit MotionInitiated(id);
