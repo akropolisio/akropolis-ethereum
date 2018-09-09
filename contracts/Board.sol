5e84f67f038bf5d6e5ce4c086ed0c2e5c8c35019
Wafik Salim <wafiks@gmail.com>
2018-09-09 11:18:30 +1000

    Vote functions return tally

--- contracts/Board.sol
+++ contracts/Board.sol
@@ -165,7 +165,7 @@ contract Board is BytesHandler {
         return motion;
     }
 
-    function _isVotable(MotionStatus status) 
+    function _isVotable(MotionStatus status)
         internal
         pure
         returns (bool)
@@ -387,7 +387,7 @@ contract Board is BytesHandler {
         return motion.expiry < now;
     }
 
-    function motionPastExpiry(uint motionID) 
+    function motionPastExpiry(uint motionID)
         public
         view
         returns (bool)
@@ -412,7 +412,7 @@ contract Board is BytesHandler {
         _expireMotion(motion);
     }
 
-    function motionPasses(uint motionID) 
+    function motionPasses(uint motionID)
         public
         view
         returns (bool)
@@ -420,7 +420,7 @@ contract Board is BytesHandler {
         return _motionPasses(_getActiveMotion(motionID));
     }
 
-    function motionFails(uint motionID) 
+    function motionFails(uint motionID)
         public
         view
         returns (bool)
@@ -447,17 +447,18 @@ contract Board is BytesHandler {
     function voteForMotion(uint motionID)
         public
         onlyDirectors
+        returns (uint votesFor, uint votesAgainst)
     {
         Motion storage motion = _getVotableMotion(motionID);
 
         if (_motionPastExpiry(motion)) {
             _expireMotion(motion);
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         VoteType existingVote = motion.vote[msg.sender];
         if (existingVote == VoteType.Yes) {
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         motion.vote[msg.sender] = VoteType.Yes;
@@ -465,27 +466,31 @@ contract Board is BytesHandler {
         if (existingVote == VoteType.No) {
             motion.votesAgainst--;
         }
+
         emit VoteCast(motionID, msg.sender, VoteType.Yes);
 
         if (_motionPasses(motion)) {
             motion.status = MotionStatus.Passed;
         }
+
+        return (motion.votesFor, motion.votesAgainst);
     }
 
     function voteAgainstMotion(uint motionID)
         public
         onlyDirectors
+        returns (uint votesFor, uint votesAgainst)
     {
         Motion storage motion = _getVotableMotion(motionID);
 
         if (_motionPastExpiry(motion)) {
             _expireMotion(motion);
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         VoteType existingVote = motion.vote[msg.sender];
         if (existingVote == VoteType.No) {
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         motion.vote[msg.sender] = VoteType.No;
@@ -498,24 +503,27 @@ contract Board is BytesHandler {
         if (_motionFails(motion)) {
             motion.status = MotionStatus.Failed;
         }
+
+        return (motion.votesFor, motion.votesAgainst);
     }
 
     function abstainFromMotion(uint motionID)
         public
         onlyDirectors
+        returns (uint votesFor, uint votesAgainst)
     {
         Motion storage motion = _getVotableMotion(motionID);
 
         if (_motionPastExpiry(motion)) {
             _expireMotion(motion);
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         VoteType existingVote = motion.vote[msg.sender];
         motion.vote[msg.sender] = VoteType.Abstain;
         if (existingVote == VoteType.Abstain ||
             existingVote == VoteType.Absent) {
-            return;
+            return (motion.votesFor, motion.votesAgainst);
         }
 
         if (existingVote == VoteType.Yes) {
@@ -528,6 +536,8 @@ contract Board is BytesHandler {
         if (!(_motionPasses(motion) || _motionFails(motion))) {
             motion.status = MotionStatus.Active;
         }
+
+        return (motion.votesFor, motion.votesAgainst);
     }
 
     event Resigned(address indexed director);
